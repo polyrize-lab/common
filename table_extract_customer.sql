25b34b9cd2ad2b1343a852ae4330f0c4424f9e51
Josh Hammond <josh@polyrizelab.com>
2025-02-07 11:00:32 -0500

    Add files via upload

---
+++ table_extract_customer.sql
@@ -0,0 +1,10 @@
+{
+  "patient_id": "A0987",
+  "name": "Lena Moore",
+  "dob": "1992-05-16",
+  "ssn": "850-42-7589",
+  "diagnosis": "Type 2 Diabetes",
+  "medications": ["Metformin 500mg", "Lisinopril 20mg"],
+  "prescribing_doctor": "Dr. Jessica Owens",
+  "contact": "+1 (321) 555-7645"
+}
