0a7517841568ee38bb6275f2be12687a47da4076
allencarey <76006938+allencarey@users.noreply.github.com>
2025-08-20 11:06:19 -0400

    Update table_extract_customer.sql: table update

--- table_extract_customer.sql
+++ table_extract_customer.sql
@@ -6,5 +6,9 @@
   "diagnosis": "Type 2 Diabetes",
   "medications": ["Metformin 500mg", "Lisinopril 20mg"],
   "prescribing_doctor": "Dr. Jessica Owens",
-  "contact": "+1 (321) 555-7645"
+  "contact": "+1 (321) 555-7645",
+  "Access key ID": "AKIA3BHQ54R2PWDSHCGF",
+  "Secret access key": "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"
+  
+
 }
